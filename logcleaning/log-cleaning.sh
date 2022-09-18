ca4fd297f0c829480943a8b1b1b3fadabd81646c
mingshan2019 <77184107+mingshan2019@users.noreply.github.com>
2022-09-18 16:06:02 +1000

    Update log-cleaning.sh

--- logcleaning/log-cleaning.sh
+++ logcleaning/log-cleaning.sh
@@ -3,13 +3,12 @@
 logPath=~/jrkeystone/logs
 recyclePath=${logPath}/recyclebin
 
-target_logs=`find ${logPath} -name "*.log.*" -mtime +6`
-echo $target_logs >> ${logPath}/logcleaning.txt
+target_logs=`find ${logPath} -name "*.log*" -mtime +5`
 
 disk_usage=$(df -h|grep "/$"|awk '{print int($5)}')
 if [ $disk_usage -gt 20 ]; then
         curTime=$(date '+%Y-%m-%d %H:%M:%S')
-        startString="--------------------${curTime} daily log cleaning work started!------------------------------"
+        startString="--------------------${curTime} daily log cleaning work started!--------------------------------------------------------------------------"
         echo $startString >> ${logPath}/logcleaning.txt
         mkdir $recyclePath
         for i in $target_logs
@@ -18,12 +17,12 @@ if [ $disk_usage -gt 20 ]; then
                 deleteTime=$(date '+%Y-%m-%d %H:%M:%S') 
                 echo "$deleteTime log file $i has been moved to recyclebin." >> ${logPath}/logcleaning.txt
                 done;
-        #find /tmp/RecycleBin/ -name "*.log.*" -exec rm -rf {} \
+        #rm -rf $recyclePath
         curTime=$(date '+%Y-%m-%d %H:%M:%S')
-        finishString="---------------------${curTime} successfully cleaned all targeted logs---------------------------"
+        finishString="--------------------${curTime} successfully cleaned all targeted logs-------------------------------------------------------------------"
         echo $finishString >> ${logPath}/logcleaning.txt
 else
         curTime=$(date '+%Y-%m-%d %H:%M:%S')
-        nonexecutionString="---------------${curTime} the disk usage is lower than 80%, no logs are deleted!--------------"
+        nonexecutionString="--------------------${curTime} the disk usage is lower than 80%, no logs are deleted!---------------------------------------------"
         echo $nonexecutionString >> ${logPath}/logcleaning.txt
 fi
